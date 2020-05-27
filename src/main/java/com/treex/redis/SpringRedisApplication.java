69e0e12bbed0ced0b990a45a2d8d3c9d577ab0c0
seymurm <manafov_seymur@yahoo.com>
2020-05-27 02:32:19 +0200

    RestTemplate added

--- src/main/java/com/treex/redis/SpringRedisApplication.java
+++ src/main/java/com/treex/redis/SpringRedisApplication.java
@@ -2,14 +2,19 @@ package com.treex.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class SpringRedisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringRedisApplication.class, args);
     }
 
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
