00c2cc66ac2fdba460bc8f38ee287dbf6277f914
seymurm <manafov_seymur@yahoo.com>
2020-05-27 02:30:51 +0200

    Task endpoint added

---
+++ src/main/java/com/treex/redis/controller/TaskController.java
@@ -0,0 +1,56 @@
+package com.treex.redis.controller;
+
+import com.treex.redis.model.Task;
+import com.treex.redis.model.User;
+import com.treex.redis.service.UserRepository;
+import com.treex.redis.service.web.TaskFacade;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.security.SecureRandom;
+import java.util.List;
+
+/**
+ * Created by seymurmanafov on 2020. 05. 25..
+ */
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final UserRepository userRepository;
+    private final RedisTemplate<String, User> redisTemplate;
+    private final TaskFacade taskFacade;
+
+    public TaskController(UserRepository userRepository, RedisTemplate<String, User> redisTemplate, TaskFacade taskFacade) {
+        this.userRepository = userRepository;
+        this.redisTemplate = redisTemplate;
+        this.taskFacade = taskFacade;
+    }
+
+    @GetMapping
+    public List<Task> getTasks(Principal principal) {
+        User user = userRepository.findByUsername(principal.getName());
+
+        SecureRandom random = new SecureRandom();
+        String requestId = String.valueOf(random.nextInt(100000));
+
+        redisTemplate.opsForHash().put(requestId, "user", user);
+        return taskFacade.getTasks(requestId);
+    }
+
+    @PostMapping
+    public void addTask(Principal principal) {
+        User user = userRepository.findByUsername(principal.getName());
+
+        SecureRandom random = new SecureRandom();
+        String requestId = String.valueOf(random.nextInt(100000));
+
+        redisTemplate.opsForHash().put(requestId, "user", user);
+        taskFacade.addTasks(requestId);
+
+    }
+
+}
